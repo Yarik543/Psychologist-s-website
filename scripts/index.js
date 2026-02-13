b10b524c72491d342f50b50c207fe2e1036a5520
Yarik543 <aroslavkasymbekov@gmail.com>
2026-02-13 19:07:26 +0300

    Адаптивность 4 блок

--- scripts/index.js
+++ scripts/index.js
@@ -30,3 +30,36 @@ cards.addEventListener("scroll", () => {
 
 // стартовое состояние
 setActive(0);
+
+const cardsAdvantages = document.querySelector(".fourth-block .card-advantages");
+const dotsAdvantages = document.querySelectorAll(".slider-dots-Advantages label");
+const radiosAdvantages = document.querySelectorAll(
+    '.fourth-block input[type="radio"]',
+);
+
+function setActiveAdvantages(index) {
+    dotsAdvantages.forEach((dot, i) => {
+        dot.classList.toggle("activeDot", i === index);
+        radiosAdvantages[i].checked = i === index;
+    });
+}
+
+// клик по точкам
+dotsAdvantages.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        cardsAdvantages.scrollTo({
+            left: cardsAdvantages.offsetWidth * index,
+            behavior: "smooth",
+        });
+        setActiveAdvantages(index);
+    });
+});
+
+// свайп
+cardsAdvantages.addEventListener("scroll", () => {
+    const index = Math.round(cardsAdvantages.scrollLeft / cardsAdvantages.offsetWidth);
+    setActiveAdvantages(index);
+});
+
+//Start
+setActiveAdvantages(0);
