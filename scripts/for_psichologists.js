27203aa019b85e867a2674195af01999b90ea5fc
Yarik543 <aroslavkasymbekov@gmail.com>
2026-02-26 21:11:31 +0300

    Блок 3 адаптив стр. 4 готов

--- scripts/for_psichologists.js
+++ scripts/for_psichologists.js
@@ -16,3 +16,38 @@ const closeBurger = () => {
 
 burgerClose.addEventListener("click", closeBurger);
 burgerOverlay.addEventListener("click", closeBurger);
+
+
+//слайдер CardAdvantages
+const cardExpectations = document.querySelector(".card-expectations");
+const dotsAdvantages = document.querySelectorAll(".slider-dots label");
+const radiosAdvantages = document.querySelectorAll(
+    '.section-third input[type="radio"]',
+);
+
+let setActiveExpectations = (index) => {
+    dotsAdvantages.forEach((dot, i) => {
+        dot.classList.toggle("activeDot", i === index);
+        radiosAdvantages[i].checked = i === index;
+    });
+}
+
+// клик по точкам
+dotsAdvantages.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        cardExpectations.scrollTo({
+            left: cardExpectations.offsetWidth * index,
+            behavior: "smooth",
+        });
+        setActiveExpectations(index);
+    });
+});
+
+// свайп
+cardExpectations.addEventListener("scroll", () => {
+    const index = Math.round(cardExpectations.scrollLeft / cardExpectations.offsetWidth);
+    setActiveExpectations(index);
+});
+
+//Start
+setActiveExpectations(0);
